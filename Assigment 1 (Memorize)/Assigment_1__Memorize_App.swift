6696674583ff7bef81b2e48628107e0d7f203c85
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-03-22 11:21:06 +0100

    Initial Commit

---
+++ Assigment 1 (Memorize)/Assigment_1__Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Assigment_1__Memorize_App.swift
+//  Assigment 1 (Memorize)
+//
+//  Created by Авазбек Надырбек уулу on 22.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Assigment_1__Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
